77142d65e7d27a232a71aec15799afb7cb69d693
zhjscut <806205254@qq.com>
2019-11-13 10:48:23 +0800

    201512-1 score 100

--- main.cpp
+++ main.cpp
@@ -3,22 +3,15 @@
 using namespace std;
 
 int main() {
-    int n;
+    long n;
     cin >> n;
-    int sale_2d_ago, sale_1d_ago, sale_today;
-    int cnt = 0;
-    for (unsigned i = 0; i != n; ++i) {
-        if (i == 0) { cin >> sale_2d_ago; continue; }
-        else if (i == 1) { cin >> sale_1d_ago; continue; }
-        cin >> sale_today;
-        if ( ( (sale_1d_ago > sale_2d_ago) && sale_1d_ago > sale_today) ||
-             (sale_1d_ago < sale_2d_ago && sale_1d_ago < sale_today) )
-             ++cnt;
-        sale_2d_ago = sale_1d_ago;
-        sale_1d_ago = sale_today;
+    int sum = 0;
+    while (n != 0) {
+        sum += n % 10;
+        n /= 10;
     }
     
-    cout << cnt << endl;
+    cout << sum << endl;
     
     return 0;
 }
