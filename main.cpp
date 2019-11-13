fece0d8057c9922481ce292a018cd15f7e2ae9df
zhjscut <806205254@qq.com>
2019-11-13 10:41:37 +0800

    201604-1 score 100

--- main.cpp
+++ main.cpp
@@ -5,16 +5,23 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    int prev_price, today_price;
-    cin >> prev_price;
-    int diff, max_diff = 0;
-    for (unsigned i = 1; i != n; ++i) {
-        cin >> today_price;
-        diff = abs(today_price - prev_price);
-        if (diff > max_diff) { max_diff = diff; }
-        prev_price = today_price;
+    int sale_2d_ago, sale_1d_ago, sale_today;
+    int cnt = 0;
+    for (unsigned i = 0; i != n; ++i) {
+        if (i == 0) { cin >> sale_2d_ago; continue; }
+        else if (i == 1) { cin >> sale_1d_ago; continue; }
+        cin >> sale_today;
+        if ( ( (sale_1d_ago > sale_2d_ago) && sale_1d_ago > sale_today) ||
+             (sale_1d_ago < sale_2d_ago && sale_1d_ago < sale_today) )
+             ++cnt;
+        sale_2d_ago = sale_1d_ago;
+        sale_1d_ago = sale_today;
     }
-    cout << max_diff << endl;
-            
+    
+    cout << cnt << endl;
+    
     return 0;
 }
+
+
+
