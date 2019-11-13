030e4a8ba1d43b8d2d25af38b089074837381eb1
zhjscut <806205254@qq.com>
2019-11-13 11:12:06 +0800

    201503-1 score 100

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+int matrix[1000][1000] = {};
 int main() {
-    int n;
-    cin >> n;
-    int num, prev_num;
-    cin >> prev_num;
-    int cnt = 1;
-    for (unsigned i = 1; i != n; ++i) {
-        cin >> num;
-        if (num != prev_num) {
-            ++cnt;
-            prev_num = num;
+
+    int row, col;
+    cin >> row >> col;
+    for (unsigned i = 0; i < row; ++i) {
+        for (unsigned j = 0; j < col; ++j) {
+            cin >> matrix[i][j];
         }
     }
-    
-    cout << cnt << endl;
+    for (unsigned j = col - 1; j < -1; --j) {
+        for (unsigned i = 0; i < row; ++i) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
     
     return 0;
 }
-
-
-
