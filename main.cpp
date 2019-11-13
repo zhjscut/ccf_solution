5fc3c46a789d6649e36e89f04c82c1c2414ce17b
zhjscut <806205254@qq.com>
2019-11-13 11:19:59 +0800

    201409-1 score 100

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 #include <cmath>
 using namespace std;
 
-int visits[1000] = {};
+int nums[10000] = {};
 
 int main() {
     int n;
     cin >> n;
     int num;
+    int cnt = 0;
     for (unsigned i = 0; i != n; ++i) {
         cin >> num;
-        visits[num] += 1;
-        cout << visits[num] << " ";
+        nums[num] += 1;
     }
+    for (unsigned i = 0; i != 10000 - 1; ++i) {
+        if (nums[i] == 1 && nums[i+1] == 1)
+            ++cnt;
+    }    
+    
+    cout << cnt << endl;
     
     return 0;
 }
