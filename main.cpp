512bd3a106ef3f438b94ac9d012ecbc99b4a5fe6
zhjscut <806205254@qq.com>
2019-11-13 09:01:13 +0800

    201709-1 score 100

--- main.cpp
+++ main.cpp
@@ -4,34 +4,11 @@
 using namespace std;
 
 int main() {
-    int cnt_numbers[10000] = {};
-    int n;
-    cin >> n;    
-    int num;
-    for (unsigned i = 0; i != n; ++i) {
-        cin >> num;
-        if (cnt_numbers[num] == 1) { // has the same two numbers
-            cout << 0 << endl;
-            return 0;
-        }
-        cnt_numbers[num] = 1;
-    }
-    int min = 1e+8;
-    int prev_num = -1;
-    for (unsigned i = 0; i != 10000; ++i) {    
-        if (cnt_numbers[i] == 0) { continue; }
-        else {
-            if (prev_num == -1) { prev_num = i; continue;} // init left number and right number
-            if (i - prev_num < min) { min = i - prev_num;}
-            prev_num = i;
-        }
-        if (min == 1) {
-            cout << 1 << endl;
-            return 0;
-        }
-    }
-    cout << min << endl;
-
+    int price = 10;
+    int money;
+    cin >> money;
+    int num = money / price;
+    cout << num / 5 * 7 + (num % 5 / 3) * 4 + (num % 5 % 3) << endl;
     
     return 0;
 }
