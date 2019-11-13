e6a9dc6ecdc1b42eeccb0e6472d9ddf3e2e9dafb
zhjscut <806205254@qq.com>
2019-11-13 20:46:15 +0800

    201909-2 score 100

--- main.cpp
+++ main.cpp
@@ -9,37 +9,43 @@ using namespace std;
 
 int main() {
     int num_trees, turns;
-    cin >> num_trees >> turns;
+    cin >> num_trees;
     vector<int> apples_trees;
-    vector<int> decrease_trees;
+    vector<bool> is_drop;
     int apples_tree, decrease_one_turn;
     for (unsigned i = 0; i != num_trees; ++i) {
+        cin >> turns;
         cin >> apples_tree;
         apples_trees.push_back(apples_tree);
-        decrease_trees.push_back(0);
-        for (unsigned j = 0; j != turns; ++j) {
+        is_drop.push_back(false);
+        for (unsigned j = 0; j != turns - 1; ++j) { // 第一个数值是苹果初始数量，不是疏果数
             cin >> decrease_one_turn;
-            decrease_trees[i] += decrease_one_turn; // 负数
+            if (decrease_one_turn > 0) { // 正数，表示重新确认树上苹果数量
+                if (decrease_one_turn < apples_trees[i]) {// 说明除了疏果之外，还有自然掉落，才会导致数量不相等
+                    is_drop[i] = true;
+                }
+                apples_trees[i] = decrease_one_turn;
+            }
+            else // 负数，表示疏果数量
+                apples_trees[i] += decrease_one_turn; 
         }
-    }
-    
-    int max_decrease = 0;
-    unsigned max_ix;
-    for (unsigned i = num_trees - 1; i != -1; --i) {
-        if (-decrease_trees[i] >= max_decrease) {
-            max_decrease = -decrease_trees[i];
-            max_ix = i;
-        }
-            
     }
     
     int sum = 0;
     for (auto n: apples_trees)
         sum += n;
-    for (auto n: decrease_trees)
-        sum += n;
+        
+    int drop_trees = 0;
+    for (auto b: is_drop)
+        drop_trees += b;
+        
+    int continuous_drop = 0;
+    for (unsigned i = 0; i != num_trees; ++i) {
+        if (is_drop[(i - 1 + num_trees) % num_trees] && is_drop[i] && is_drop[(i + 1) % num_trees])
+            ++continuous_drop;    
+    }
     
-    cout << sum << " " << max_ix + 1 << " " << max_decrease << endl;
+    cout << sum << " " << drop_trees << " " << continuous_drop << endl;
         
     
     return 0;
