0dbdeaed6d671d0c55200b5a468a4909c12ce878
zhjscut <806205254@qq.com>
2019-11-13 11:24:13 +0800

    201403-1 score 100

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 
-int nums[10000] = {};
+int nums[2001] = {};
 
 int main() {
     int n;
@@ -11,10 +11,10 @@ int main() {
     int cnt = 0;
     for (unsigned i = 0; i != n; ++i) {
         cin >> num;
-        nums[num] += 1;
+        nums[num + 1000] += 1;
     }
-    for (unsigned i = 0; i != 10000 - 1; ++i) {
-        if (nums[i] == 1 && nums[i+1] == 1)
+    for (unsigned i = 0; i != 1000; ++i) {
+        if (nums[i] == 1 && nums[2000 - i] == 1)
             ++cnt;
     }    
     
