76583ca4a7edcd6a6079f5e8d96d2d2fc766ecaa
zhjscut <806205254@qq.com>
2019-11-13 19:55:35 +0800

    201909-1 redo score 100

--- main.cpp
+++ main.cpp
@@ -8,28 +8,39 @@
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> numbers;
-    int num;
-    int max, min;
-    double mid;
-
-    for (unsigned i = 0; i != n; ++i) {
-        cin >> num;
-        numbers.push_back(num);
+    int num_trees, turns;
+    cin >> num_trees >> turns;
+    vector<int> apples_trees;
+    vector<int> decrease_trees;
+    int apples_tree, decrease_one_turn;
+    for (unsigned i = 0; i != num_trees; ++i) {
+        cin >> apples_tree;
+        apples_trees.push_back(apples_tree);
+        decrease_trees.push_back(0);
+        for (unsigned j = 0; j != turns; ++j) {
+            cin >> decrease_one_turn;
+            decrease_trees[i] += decrease_one_turn; // 负数
+        }
     }
-    if (numbers[0] > numbers[n-1]) { max = numbers[0]; min = numbers[n-1]; }
-    else { max = numbers[n-1]; min = numbers[0]; }        
-    if (n % 2 == 0) { 
-        mid = double(numbers[n/2 - 1] + numbers[n/2]) / 2; 
-    }
-    else { mid = numbers[n/2]; }
     
-    if (n % 2 == 0 && (numbers[n/2 - 1] + numbers[n/2]) % 2 == 1)
-        printf("%d %.1lf %d", max, mid, min);
-    else
-        printf("%d %d %d", max, int(mid), min);
-
+    int max_decrease = 0;
+    unsigned max_ix;
+    for (unsigned i = num_trees - 1; i != -1; --i) {
+        if (-decrease_trees[i] >= max_decrease) {
+            max_decrease = -decrease_trees[i];
+            max_ix = i;
+        }
+            
+    }
+    
+    int sum = 0;
+    for (auto n: apples_trees)
+        sum += n;
+    for (auto n: decrease_trees)
+        sum += n;
+    
+    cout << sum << " " << max_ix + 1 << " " << max_decrease << endl;
+        
+    
     return 0;
 }
