43b75c006cbfabb6c00961bbf862a3a0fc5eee43
zhjscut <806205254@qq.com>
2019-11-13 09:13:01 +0800

    201703-1 score 100

--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@
 using namespace std;
 
 int main() {
-    int price = 10;
-    int money;
-    cin >> money;
-    int num = money / price;
-    cout << num / 5 * 7 + (num % 5 / 3) * 4 + (num % 5 % 3) << endl;
+    int n_friends = 0;
+    int n_cakes, min_weight;
+    int weight, weight_i;
+    cin >> n_cakes >> min_weight;
+    int i_cake = 0;
+    while (i_cake != n_cakes) {
+        if (weight == 0) {
+            n_friends += 1;
+            cin >> weight;
+        }
+        else {
+            cin >> weight_i;  
+            weight += weight_i;                      
+        }
+        if (weight >= min_weight)
+            weight = 0;
+        ++i_cake;
+    }
+    cout << n_friends << endl;
     
     return 0;
 }
