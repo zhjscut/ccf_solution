5ada7323b7afe0f1b06daece909b86f20430a291
zhjscut <806205254@qq.com>
2019-11-13 08:50:53 +0800

    201712-1 score 100

--- main.cpp
+++ main.cpp
@@ -4,25 +4,34 @@
 using namespace std;
 
 int main() {
-    int score = 1, total_score = 0;
-    int status;
-    while (cin >> status) {
-        if (status == 0) {
-            cout << total_score;
-            break;
+    int cnt_numbers[10000] = {};
+    int n;
+    cin >> n;    
+    int num;
+    for (unsigned i = 0; i != n; ++i) {
+        cin >> num;
+        if (cnt_numbers[num] == 1) { // has the same two numbers
+            cout << 0 << endl;
+            return 0;
         }
-        else if (status == 1) {
-            score = 1;
-            total_score += score;
+        cnt_numbers[num] = 1;
+    }
+    int min = 1e+8;
+    int prev_num = -1;
+    for (unsigned i = 0; i != 10000; ++i) {    
+        if (cnt_numbers[i] == 0) { continue; }
+        else {
+            if (prev_num == -1) { prev_num = i; continue;} // init left number and right number
+            if (i - prev_num < min) { min = i - prev_num;}
+            prev_num = i;
         }
-        else if (status == 2) {
-            if (score == 1 || total_score == 0)
-                score = 2;
-            else
-                score += 2;
-            total_score += score;
+        if (min == 1) {
+            cout << 1 << endl;
+            return 0;
         }
     }
-        
+    cout << min << endl;
+
+    
     return 0;
 }
