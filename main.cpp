20c6429eb02529b5fe45641501f40fe33c8eddc4
zhjscut <806205254@qq.com>
2019-11-14 10:56:17 +0800

    201812-2 score 80 out of time

--- main.cpp
+++ main.cpp
@@ -1,222 +1,55 @@
 #include <iostream>
-#include <cmath>
-#include <iomanip>
+#include <sstream>
 #include <vector>
-
-#include <stdio.h>
-
 using namespace std;
 
 int main() {
-  //  int num1, num2, num3, num4;
-//    char ch1, ch2, ch3;
-    int num[4];
-    char ch[3];
+    int r, y, g;
+    int light[3];
+    cin >> r >> y >> g;
     int n;
     cin >> n;
-    int ans;
-    int compute(int num[4], char ch[3]); 
-   // cin >> num1 >> ch1 >> num2 >> ch2 >> num3 >> ch3 >> num4;   
-    //cout << num1 << " " << ch1 << " " << num2 << " " << ch2 << " " << num3 << " " << ch3 << " "
-      //   << num4 << endl;     
-    for (unsigned i = 0; i != n; ++i) {
-        cin >> num[0] >> ch[0] >> num[1] >> ch[1] >> num[2] >> ch[2] >> num[3];  
-       // cout << num[0]<< " " << ch[0] << " " << num[1] << " " << ch[1] << " " << num[2] << " " 
-         //    << ch[2] << " " << num[3] << endl;  
-        
-        ans = compute(num, ch);             
-        /*
-        ans = num[0];
-        for (unsigned j = 0; j != 3; ++j) {
-            if (ch[j] == '+') { ans += num[j + 1]; }
-            else if (ch[j] == '-') { ans -= num[j + 1]; }
-            else { ; }
-        }
-        */
-        if (ans == 24)
-            cout << "Yes" << endl;
-        else
-            cout << "No" << endl;        
-    }
-
+    //cin >> light[0] >> light[2] >> light[1] >> n;
+    int type, time;
+    long long total_time = 0;
+    int interval[3] = {r, y, g};
+    void update(int [3], long long, int &, int &);    
+    int sum = light[0] + light[1] + light[2];
     
+    for (int i = 0; i != n; ++i) {
+        cin >> type >> time;
+      //  cout << "before: " << i << " " << type << " " << time << " " << total_time << endl;        
+        update(interval, total_time, type, time);
+      //  cout << "after: " << i << " " << type << " " << time << endl;
+        if (type == 0) { total_time += time; }
+        if (type == 1) { total_time += time; }
+        if (type == 2) { total_time += time + r; } 
+        if (type == 3) { ; } 
+    }
+    
+    cout << total_time;    
     return 0;
 }
 
-int compute(int num[4], char ch[3]) 
-{   
-    int ans;
-    if (ch[0] == '+') {
-        if (ch[1] == '+') {
-            if (ch[2] == '+') {
-                ans = num[0] + num[1] + num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] + num[1] + num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] + num[1] + num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] + num[1] + num[2] / num[3];
-            }
-        } else if (ch[1] == '-') {
-            if (ch[2] == '+') {
-                ans = num[0] + num[1] - num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] + num[1] - num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] + num[1] - num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] + num[1] - num[2] / num[3];
-            }
-        } else if (ch[1] == 'x') {
-            if (ch[2] == '+') {
-                ans = num[0] + num[1] * num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] + num[1] * num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] + num[1] * num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] + num[1] * num[2] / num[3];
-            }
-        } else if (ch[1] == '/') {
-            if (ch[2] == '+') {
-                ans = num[0] + num[1] / num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] + num[1] / num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] + num[1] / num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] + num[1] / num[2] / num[3];
-            }
-        }
-    } else if (ch[0] == '-') {
-        if (ch[1] == '+') {
-            if (ch[2] == '+') {
-                ans = num[0] - num[1] + num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] - num[1] + num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] - num[1] + num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] - num[1] + num[2] / num[3];
-            }
-        } else if (ch[1] == '-') {
-            if (ch[2] == '+') {
-                ans = num[0] - num[1] - num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] - num[1] - num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] - num[1] - num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] - num[1] - num[2] / num[3];
-            }
-        } else if (ch[1] == 'x') {
-            if (ch[2] == '+') {
-                ans = num[0] - num[1] * num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] - num[1] * num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] - num[1] * num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] - num[1] * num[2] / num[3];
-            }
-        } else if (ch[1] == '/') {
-            if (ch[2] == '+') {
-                ans = num[0] - num[1] / num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] - num[1] / num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] - num[1] / num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] - num[1] / num[2] / num[3];
-            }
-        }
-    } else if (ch[0] == 'x') {
-        if (ch[1] == '+') {
-            if (ch[2] == '+') {
-                ans = num[0] * num[1] + num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] * num[1] + num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] * num[1] + num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] * num[1] + num[2] / num[3];
-            }
-        } else if (ch[1] == '-') {
-            if (ch[2] == '+') {
-                ans = num[0] * num[1] - num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] * num[1] - num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] * num[1] - num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] * num[1] - num[2] / num[3];
-            }
-        } else if (ch[1] == 'x') {
-            if (ch[2] == '+') {
-                ans = num[0] * num[1] * num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] * num[1] * num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] * num[1] * num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] * num[1] * num[2] / num[3];
-            }
-        } else if (ch[1] == '/') {
-            if (ch[2] == '+') {
-                ans = num[0] * num[1] / num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] * num[1] / num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] * num[1] / num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] * num[1] / num[2] / num[3];
-            }
-        }
-    } else if (ch[0] == '/') {
-        if (ch[1] == '+') {
-            if (ch[2] == '+') {
-                ans = num[0] / num[1] + num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] / num[1] + num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] / num[1] + num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] / num[1] + num[2] / num[3];
-            }
-        } else if (ch[1] == '-') {
-            if (ch[2] == '+') {
-                ans = num[0] / num[1] - num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] / num[1] - num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] / num[1] - num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] / num[1] - num[2] / num[3];
-            }
-        } else if (ch[1] == 'x') {
-            if (ch[2] == '+') {
-                ans = num[0] / num[1] * num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] / num[1] * num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] / num[1] * num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] / num[1] * num[2] / num[3];
-            }
-        } else if (ch[1] == '/') {
-            if (ch[2] == '+') {
-                ans = num[0] / num[1] / num[2] + num[3];
-            } else if (ch[2] == '-') {
-                ans = num[0] / num[1] / num[2] - num[3];
-            } else if (ch[2] == 'x') {
-                ans = num[0] / num[1] / num[2] * num[3];
-            } else if (ch[2] == '/') {
-                ans = num[0] / num[1] / num[2] / num[3];
-            }
-        }
+void update(int interval[3], long long total_time, int &type, int &time)
+{
+    if (type == 0)
+        return;
+   // 加上这个会导致很多错误，只剩30分 
+  //  int sum = interval[0] + interval[1] + interval[2];
+  //  while (total_time > sum)
+  //      total_time -= sum;
+    
+    while (total_time > time) {
+      //  cout << total_time << endl;
+        total_time -= time; 
+        type = (type - 2 + 3) % 3 + 1; // 为了实现3 -> 2, 2 -> 1, 1 -> 3的转换
+        time = interval[type - 1];       
     }
     
-    return ans;
+    time = time - total_time;
+   
+    return;
 }
 
 
