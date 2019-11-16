2eafab9d93162a3f87af80e7264c1420a619e8dc
zhjscut <806205254@qq.com>
2019-11-16 15:39:51 +0800

    201612-2 score 100

--- main.cpp
+++ main.cpp
@@ -3,44 +3,18 @@
 #include <vector>
 using namespace std;
 
-struct Child {
-	int id;
-	Child *prev;
-	Child *next;		
-};
-	
-
 int main() {
-	int n, k;
-	cin >> n >> k;
-	vector<Child> vchild;
-	for (int i = 0; i != n; ++i) {
-		Child child = Child{i + 1};
-		vchild.push_back(child);
-	}
-	for (int i = 0; i != n; ++i) {
-		vchild[i].next = &vchild[(i + 1) % n]; 	
-		vchild[i].prev = &vchild[(i - 1 + n) % n];
-	}
-	//for (auto c: vchild)
-	  //  cout << "id: " << c.id << "prev: " << c.prev->id << "next: " << c.next->id << endl;
-	int cnt = 1, nn = n;
-	Child *pchild = &vchild[0], *tmp_pchild = 0;
+	double num, ans;
+	cin >> num;
+	if (num <= 3500) { ans = num; }
+	else if (num <= 4955 ) { ans = (num - 3500) / 0.97 + 3500; }
+	else if (num <= 7655 ) { ans = (num - 4955) / 0.9 + 5000; }
+	else if (num <= 11255 ) { ans = (num - 7655) / 0.8 + 8000; }
+	else if (num <= 30755 ) { ans = (num - 11255) / 0.75 + 12500; }
+	else if (num <= 44755 ) { ans = (num - 30755) / 0.7 + 38500; }
+	else if (num <= 61005 ) { ans = (num - 44755) / 0.65 + 58500; }
+	else { ans = (num - 61005) / 0.55 + 83500; }
 	
-	while (nn != 1) {
-		//cout << nn << endl;
-		if (cnt % k == 0 || cnt % 10 == k) {
-		   // cout << pchild->id << " out." << endl;
-			pchild->prev->next = pchild->next;
-			pchild->next->prev = pchild->prev;
-			--nn;
-		}
-		pchild = pchild->next;
-		++cnt;
-	}
-	
-	cout << pchild->id << endl;
-
-    return 0;
-}
-
+	cout << int(ans) << endl;
+	return 0; 
+} 
